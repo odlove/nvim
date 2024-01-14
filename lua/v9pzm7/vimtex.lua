4f38ec0b6cf9bf9d7ca35fac6ebd8a1134a358c3
v9pzm7 <v9pzm7@arch.localdomain>
2024-01-14 23:34:17 +0800

    add vimtex build path config

--- lua/v9pzm7/vimtex.lua
+++ lua/v9pzm7/vimtex.lua
@@ -24,6 +24,10 @@ vim.g.vimtex_compiler_latexmk_engines = {
     ['context (xetex)'] = '-pdf -pdflatex=\'texexec --xtx\'',
 }
 
+vim.g.vimtex_compiler_latexmk = {
+    aux_dir = 'build',
+}
+
 vim.g.vimtex_quickfix_mode = 2
 vim.g.vimtex_quickfix_open_on_warning = 0
 vim.g.vimtex_quickfix_ignore_filters = {
